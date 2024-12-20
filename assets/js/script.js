ec5c306b23d9b0ee6dd9904981a882320b7bfe98
Mado007 <mahmodeid2019@gmail.com>
2024-12-21 00:43:08 +0200

    Fix phone lable and input validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ visToggle.addEventListener('click', function(){
   }
 password = !password;
 });
-
 var usernameInput = document.getElementById("username");
 var lengthRequirement = document.getElementById("lengthRequirement");
 var charRequirement = document.getElementById("charRequirement");
@@ -39,19 +38,67 @@ usernameInput.onkeyup = function () {
     charRequirement.classList.remove("valid");
   }
 };
-const emailInput = document.getElementById("email");
-const emailRequirement = document.getElementById("emailRequirement");
+var emailInput = document.getElementById("email");
+var emailRequirement = document.getElementById("emailRequirement");
 
 emailInput.addEventListener("input", function () {
-  const emailValue = emailInput.value;
+  var emailValue = emailInput.value;
   
-  const validEmailPattern = /^[\w.-]{3,20}@([\w-]+\.)+[\w-]{2,4}$/;
+  var validEmailPattern = /^[\w.-]{3,20}@([\w-]+\.)+[\w-]{2,4}$/;
   if (validEmailPattern.test(emailValue)) {
     emailRequirement.classList.add("valid");
     emailRequirement.classList.remove("invalid");
   } else {
     emailRequirement.classList.add("invalid");
     emailRequirement.classList.remove("valid");
+  }
+});
+
+var passwordInput = document.getElementById("password");
+var lengthPasswordRequirement = document.getElementById("lengthPasswordRequirement");
+var uppercaseRequirement = document.getElementById("uppercaseRequirement");
+
+passwordInput.onkeyup = function () {
+  var validLength = passwordInput.value.length >= 8 && passwordInput.value.length <= 100;
+  var validUppercase = /[A-Z]/.test(passwordInput.value);
+
+  if (validLength) {
+    lengthPasswordRequirement.classList.add("valid");
+    lengthPasswordRequirement.classList.remove("invalid");
+  } else {
+    lengthPasswordRequirement.classList.add("invalid");
+    lengthPasswordRequirement.classList.remove("valid");
+  }
+
+  if (validUppercase) {
+    uppercaseRequirement.classList.add("valid");
+    uppercaseRequirement.classList.remove("invalid");
+  } else {
+    uppercaseRequirement.classList.add("invalid");
+    uppercaseRequirement.classList.remove("valid");
+  }
+};
+var phoneInput = document.getElementById("phoneNumber");
+var lengthPhoneRequirement = document.getElementById("lengthPhoneRequirement");
+var numRequirement = document.getElementById("numRequirement");
+
+phoneInput.addEventListener("input", function () {
+  var phoneValue = phoneInput.value;
+
+  if (phoneValue.length === 11) {
+    lengthPhoneRequirement.classList.add("valid");
+    lengthPhoneRequirement.classList.remove("invalid");
+  } else {
+    lengthPhoneRequirement.classList.add("invalid");
+    lengthPhoneRequirement.classList.remove("valid");
+  }
+
+  if (/^(011|012|015)/.test(phoneValue)) {
+    numRequirement.classList.add("valid");
+    numRequirement.classList.remove("invalid");
+  } else {
+    numRequirement.classList.add("invalid");
+    numRequirement.classList.remove("valid");
   }
 });
 document.querySelector("form").addEventListener("submit", function (event) {
